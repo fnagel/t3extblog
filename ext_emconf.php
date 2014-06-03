e8c7b371e12f14f2fcb11cb06f12e623ad78cbb6
Felix Nagel <info@felixnagel.com>
2014-06-03 18:05:02 +0200

    [TSAK] update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,12 +27,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.8.0',
+	'version' => '0.9.0',
 	'constraints' => array(
 		'depends' => array(
 			'extbase' => '1.5',
 			'fluid' => '1.5',
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 			't3blog' => '',
