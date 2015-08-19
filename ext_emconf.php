6b36a69852f8642028dca967d3a288231a33886e
Felix Nagel <info@felixnagel.com>
2015-08-20 01:07:25 +0200

    [TASK] Consider extension TYPO3 CMS 7.4 ready

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.3.99',
+			'typo3' => '6.2.0-7.4.99',
 			'extbase' => '',
 			'fluid' => '',
 		),
