041938ce2612ddd662fbd15c2cc38e2f34fa5310
Felix Nagel <info@felixnagel.com>
2015-12-18 15:08:21 +0100

    [FIX] Remove overwrite for German localization
    Related to #99
    This reverts commit 1a5befbada4ef65c095336d5bf6b52e2d3c69176.

--- ext_localconf.php
+++ ext_localconf.php
@@ -143,10 +143,6 @@ $requiredParameters = array(
 );
 $GLOBALS['TYPO3_CONF_VARS']['FE']['cHashRequiredParameters'] .= ',' . implode(',', $requiredParameters);
 
-// @todo Remove this, see: https://github.com/fnagel/t3extblog/issues/99
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:t3extblog/Resources/Private/Language/locallang.xlf'][] =
-	'EXT:t3extblog/Resources/Private/Language/de.locallang.xlf';
-
 // Make default avatar provider abailable in FE
 // @todo Remove this when 6.2 is no longer relevant
 if (version_compare(TYPO3_branch, '7.5', '>=') && TYPO3_MODE == 'FE') {
