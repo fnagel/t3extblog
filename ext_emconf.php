42e2f81134662f8154174b576d97ca5aa40def25
Felix Nagel <info@felixnagel.com>
2016-04-06 23:03:31 +0200

    [TASK] Adjust realurl conflict constraint
    Closes #125

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'conflicts' => array(
 			't3blog' => '',
-			'realurl' => '2.0.0-2.99.99',
+			'realurl' => '2.0.0-2.0.10',
 		),
 		'suggests' => array(
 			'dd_googlesitemap' => '2.0.0-2.0.99',
