2e3fc68fc116822e7b134fb91f5fb397e6a0f658
Felix Nagel <info@felixnagel.com>
2018-03-01 21:57:08 +0100

    [TASK] Extract link validator config to own file

---
+++ Configuration/TSconfig/LinkValidator.ts
@@ -0,0 +1,8 @@
+# Link validator config
+mod.linkvalidator.searchFields {
+	tx_t3blog_cat = description
+	tx_t3blog_com = text, website
+	tx_t3blog_post = preview_text, trackback
+	tx_t3blog_pingback = url
+	tx_t3blog_trackback = fromurl
+}
--- Configuration/TSconfig/Page.ts
+++ Configuration/TSconfig/Page.ts
@@ -1,21 +1,16 @@
 # Set preview text height
 RTE.config.tx_t3blog_post.preview_text.RTEHeightOverride = 300
 
+# Hide number of views property
 TCEFORM.tx_t3blog_post {
 	# See https://github.com/fnagel/t3extblog/issues/22
 	number_views.disabled = 1
-}
-
-# Link validator config
-mod.linkvalidator.searchFields {
-	tx_t3blog_cat = description
-	tx_t3blog_com = text, website
-	tx_t3blog_post = preview_text, trackback
-	tx_t3blog_pingback = url
-	tx_t3blog_trackback = fromurl
 }
 
 # Hide tt_content elements in list view
 [page|module = t3blog]
 	mod.web_list.table.tt_content.hideTable = 1
 [global]
+
+# Includes
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3extblog/Configuration/TSconfig/LinkValidator.ts">
