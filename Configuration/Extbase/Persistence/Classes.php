578ec964d5716f076aa5a657105862ed1fa6451c
Felix Nagel <info@felixnagel.com>
2020-01-07 21:18:55 +0100

    [FEATURE] Use latest table mapping configuration
    Needed for TYPO3 10.
    
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,240 @@
+<?php
+
+return [
+    \FelixNagel\T3extblog\Domain\Model\Post::class => [
+        'tableName' => 'tx_t3blog_post',
+        'properties' => [
+            'crdate' => [
+                'fieldName' => 'crdate'
+            ],
+            'tstamp' => [
+                'fieldName' => 'tstamp'
+            ],
+            'sorting' => [
+                'fieldName' => 'sorting'
+            ],
+            'deleted' => [
+                'fieldName' => 'deleted'
+            ],
+            'hidden' => [
+                'fieldName' => 'hidden'
+            ],
+
+            'title' => [
+                'fieldName' => 'title'
+            ],
+            'author' => [
+                'fieldName' => 'link'
+            ],
+            'publishDate' => [
+                'fieldName' => 'date'
+            ],
+            'content' => [
+                'fieldName' => 'content'
+            ],
+            'allowComments' => [
+                'fieldName' => 'allow_comments'
+            ],
+            'categories' => [
+                'fieldName' => 'cat'
+            ],
+            'tagCloud' => [
+                'fieldName' => 'tagClouds'
+            ],
+            'numberOfViews' => [
+                'fieldName' => 'number_views'
+            ],
+            'mailsSent' => [
+                'fieldName' => 'mails_sent'
+            ],
+            'metaDescription' => [
+                'fieldName' => 'meta_description'
+            ],
+            'metaKeywords' => [
+                'fieldName' => 'meta_keywords'
+            ],
+            'previewMode' => [
+                'fieldName' => 'preview_mode'
+            ],
+            'previewText' => [
+                'fieldName' => 'preview_text'
+            ],
+            'previewImage' => [
+                'fieldName' => 'preview_image'
+            ],
+        ],
+    ],
+
+    \FelixNagel\T3extblog\Domain\Model\Content::class => [
+        'tableName' => 'tt_content',
+        'properties' => [
+            'colPos' => [
+                'fieldName' => 'colPos'
+            ],
+            'CType' => [
+                'fieldName' => 'CType'
+            ],
+        ],
+    ],
+
+    \FelixNagel\T3extblog\Domain\Model\Category::class => [
+        'tableName' => 'tx_t3blog_cat',
+        'properties' => [
+            'crdate' => [
+                'fieldName' => 'crdate'
+            ],
+            'tstamp' => [
+                'fieldName' => 'tstamp'
+            ],
+            'sorting' => [
+                'fieldName' => 'sorting'
+            ],
+            'deleted' => [
+                'fieldName' => 'deleted'
+            ],
+            'hidden' => [
+                'fieldName' => 'hidden'
+            ],
+
+            'name' => [
+                'fieldName' => 'catname'
+            ],
+            'description' => [
+                'fieldName' => 'description'
+            ],
+            'parentId' => [
+                'fieldName' => 'parent_id'
+            ],
+        ],
+    ],
+
+    \FelixNagel\T3extblog\Domain\Model\Comment::class => [
+        'tableName' => 'tx_t3blog_com',
+        'properties' => [
+            'crdate' => [
+                'fieldName' => 'crdate'
+            ],
+            'tstamp' => [
+                'fieldName' => 'tstamp'
+            ],
+            'sorting' => [
+                'fieldName' => 'sorting'
+            ],
+            'deleted' => [
+                'fieldName' => 'deleted'
+            ],
+            'hidden' => [
+                'fieldName' => 'hidden'
+            ],
+
+            'title' => [
+                'fieldName' => 'title'
+            ],
+            'postId' => [
+                'fieldName' => 'fk_post'
+            ],
+            'author' => [
+                'fieldName' => 'author'
+            ],
+            'email' => [
+                'fieldName' => 'email'
+            ],
+            'website' => [
+                'fieldName' => 'website'
+            ],
+            'date' => [
+                'fieldName' => 'date'
+            ],
+            'text' => [
+                'fieldName' => 'text'
+            ],
+            'approved' => [
+                'fieldName' => 'approved'
+            ],
+            'spam' => [
+                'fieldName' => 'spam'
+            ],
+            'mailsSent' => [
+                'fieldName' => 'mails_sent'
+            ],
+            'privacyPolicyAccepted' => [
+                'fieldName' => 'privacy_policy_accepted'
+            ],
+        ],
+    ],
+
+    \FelixNagel\T3extblog\Domain\Model\BackendUser::class => [
+        'tableName' => 'be_users',
+    ],
+
+    \FelixNagel\T3extblog\Domain\Model\PostSubscriber::class => [
+        'tableName' => 'tx_t3blog_com_nl',
+        'properties' => [
+            'crdate' => [
+                'fieldName' => 'crdate'
+            ],
+            'tstamp' => [
+                'fieldName' => 'tstamp'
+            ],
+            'deleted' => [
+                'fieldName' => 'deleted'
+            ],
+            'hidden' => [
+                'fieldName' => 'hidden'
+            ],
+
+            'name' => [
+                'fieldName' => 'name'
+            ],
+            'email' => [
+                'fieldName' => 'email'
+            ],
+            'postUid' => [
+                'fieldName' => 'post_uid'
+            ],
+            'lastSent' => [
+                'fieldName' => 'lastsent'
+            ],
+            'code' => [
+                'fieldName' => 'code'
+            ],
+            'privacyPolicyAccepted' => [
+                'fieldName' => 'privacy_policy_accepted'
+            ],
+        ],
+    ],
+
+    \FelixNagel\T3extblog\Domain\Model\BlogSubscriber::class => [
+        'tableName' => 'tx_t3blog_blog_nl',
+        'properties' => [
+            'crdate' => [
+                'fieldName' => 'crdate'
+            ],
+            'tstamp' => [
+                'fieldName' => 'tstamp'
+            ],
+            'deleted' => [
+                'fieldName' => 'deleted'
+            ],
+            'hidden' => [
+                'fieldName' => 'hidden'
+            ],
+
+            'email' => [
+                'fieldName' => 'email'
+            ],
+            'postUid' => [
+                'fieldName' => 'post_uid'
+            ],
+            'lastSent' => [
+                'fieldName' => 'lastsent'
+            ],
+            'code' => [
+                'fieldName' => 'code'
+            ],
+            'privacyPolicyAccepted' => [
+                'fieldName' => 'privacy_policy_accepted'
+            ],
+        ],
+    ],
+];
