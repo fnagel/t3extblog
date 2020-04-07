525b8cb2960c25e311514d7411bca1d2049c5848
Felix Nagel <info@felixnagel.com>
2020-04-07 16:58:59 +0200

    [TASK] Consider extension PHP 7.3 compatible
    See #224

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '5.0.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.0.0-7.2.99',
+            'php' => '7.0.0-7.3.99',
             'typo3' => '9.4.0-9.5.99',
         ],
         'conflicts' => [
