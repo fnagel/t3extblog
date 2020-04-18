df7aa1271f0374a1e3abad42c71bd7b5a7abe5ac
Felix Nagel <info@felixnagel.com>
2020-04-18 22:50:04 +0200

    [CLEANUP] Remove unused object manager from base test

--- Tests/Unit/Domain/Model/BaseTest.php
+++ Tests/Unit/Domain/Model/BaseTest.php
@@ -10,26 +10,18 @@ namespace FelixNagel\T3extblog\Tests\Unit\Domain\Model;
  */
 
 use Nimut\TestingFramework\TestCase\UnitTestCase;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
 
 /**
  * BaseTest.
  */
 abstract class BaseTest extends UnitTestCase
 {
-    /**
-     * @var ObjectManager
-     */
-    protected $objectManager;
-
     protected $fixture;
 
     /**
      */
     public function setUp()
     {
-        $this->objectManager = GeneralUtility::makeInstance(ObjectManager::class);
     }
 
     /**
