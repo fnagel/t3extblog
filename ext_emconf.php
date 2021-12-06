6cc8de7692128974723f729df82ef62f102fb584
Felix Nagel <info@felixnagel.com>
2021-12-06 01:00:19 +0100

    [!!!][TASK] Add TYPO3 11.x and remove TYPO3 10.x support

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,8 +21,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '6.2.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.4.99',
-            'typo3' => '10.4.1-10.4.99',
+            'php' => '7.4.0-8.0.99',
+            'typo3' => '11.0.0-11.5.99',
         ],
         'conflicts' => [
             't3blog' => '',
