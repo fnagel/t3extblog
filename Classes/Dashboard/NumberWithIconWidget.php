13b647d12398d49bab75f92dc0ee30761eaeb907
Felix Nagel <info@felixnagel.com>
2023-11-16 17:44:26 +0100

    [TASK] Adjust dashboard widget registration and rendering for TYPO3 v12
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-96812-NoFrontendTypoScriptBasedTemplateOverridesInTheBackend.html

--- Classes/Dashboard/NumberWithIconWidget.php
+++ Classes/Dashboard/NumberWithIconWidget.php
@@ -9,30 +9,56 @@ namespace FelixNagel\T3extblog\Dashboard;
  * LICENSE.txt file that was distributed with this source code.
  */
 
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\View\BackendViewFactory;
 use TYPO3\CMS\Dashboard\Widgets\ButtonProviderInterface;
 use TYPO3\CMS\Dashboard\Widgets\NumberWithIconDataProviderInterface;
-use TYPO3\CMS\Dashboard\Widgets\NumberWithIconWidget as CoreNumberWithIconWidget;
+use TYPO3\CMS\Dashboard\Widgets\RequestAwareWidgetInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
-use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 
 /**
- * AbstractNumberWithIconWidget
- *
  * Extends original widget with a button provider.
+ *
+ * Copied from TYPO3\CMS\Dashboard\Widgets\NumberWithIconWidget
  */
-class NumberWithIconWidget extends CoreNumberWithIconWidget
+class NumberWithIconWidget implements WidgetInterface, RequestAwareWidgetInterface
 {
+    private ServerRequestInterface $request;
+
     public function __construct(
-        WidgetConfigurationInterface $configuration,
-        NumberWithIconDataProviderInterface $dataProvider,
-        StandaloneView $view,
-        ButtonProviderInterface $buttonProvider = null,
-        array $options = []
+        private readonly WidgetConfigurationInterface $configuration,
+        private readonly NumberWithIconDataProviderInterface $dataProvider,
+        private readonly BackendViewFactory $backendViewFactory,
+        private readonly ButtonProviderInterface $buttonProvider,
+        private readonly array $options = [],
     ) {
-        parent::__construct($configuration, $dataProvider, $view, $options);
+    }
+
+    public function setRequest(ServerRequestInterface $request): void
+    {
+        $this->request = $request;
+    }
+
+    public function renderWidgetContent(): string
+    {
+        $view = $this->backendViewFactory->create($this->request, ['typo3/cms-dashboard', 'felixnagel/t3extblog']);
 
         $view->assignMultiple([
-            'button' => $buttonProvider,
+            'icon' => $this->options['icon'] ?? '',
+            'title' => $this->options['title'] ?? '',
+            'subtitle' => $this->options['subtitle'] ?? '',
+            'number' => $this->dataProvider->getNumber(),
+            'options' => $this->options,
+            'configuration' => $this->configuration,
+            'button' => $this->buttonProvider,
         ]);
+
+        return $view->render('Widget/NumberWithIconWidget');
+    }
+
+    public function getOptions(): array
+    {
+        return $this->options;
     }
 }
