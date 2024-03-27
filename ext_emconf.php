e23d6aab2010d163679ac6c3d3dab7610f0b7d07
Felix Nagel <info@felixnagel.com>
2024-03-27 17:12:08 +0100

    [!!!][TASK] Add TYPO3 13.x support

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,11 +20,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@felixnagel.com',
     'author_company' => '',
     'state' => 'stable',
-    'version' => '8.0.3-dev',
+    'version' => '9.0.0-dev',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.2.99',
-            'typo3' => '12.0.0-12.4.99',
+            'php' => '8.2.0-8.3.99',
+            'typo3' => '12.0.0-13.4.99',
             'dashboard' => '',
         ],
         'conflicts' => [
