0ddb9784ba5aa8dd53b26632f99749732be472c2
Felix Nagel <info@felixnagel.com>
2026-02-27 09:46:38 +0100

    [TASK] Make tag cloud importance threshold dynamic

--- Classes/Controller/TagController.php
+++ Classes/Controller/TagController.php
@@ -31,16 +31,36 @@ class TagController extends AbstractController
         $this->addCacheTags($this->postRepository);
 
         $tags = $this->postRepository->tagCloud($limit, $minimum);
-        // Not using shuffle as we always the same randomization
+
+        $min = PHP_INT_MAX;
+        $max = 0;
+        $tags = array_map(function ($tag) use (&$min, &$max) {
+            $tag['md5'] = md5($tag['title']);
+
+            $min = min($min, $tag['posts']);
+            $max = max($max, $tag['posts']);
+
+            return $tag;
+        }, $tags);
+
+        // Not using shuffle as we want always the same randomization
         if ($settings['random'] ?? false) {
-            $tags = array_map(function ($tag) {
-                $tag['md5'] = md5($tag['title']);
-                return $tag;
-            }, $tags);
             usort($tags, fn ($a, $b) => strcmp($a['md5'], $b['md5']));
         }
 
         $this->view->assign('tags', $tags);
+
+        $three = round(($max - $min) / 3);
+        $six = round(($three * 2) / 3);
+        $this->view->assign('statistic', [
+            'min' => $min,
+            'max' => $max,
+            'count' => count($tags),
+            'threshold' => [
+                1 => $min + $six,
+                2 => round($min + $six * 2),
+                3 => round($min + $three * 2)],
+        ]);
 
         return $this->htmlResponse();
     }
