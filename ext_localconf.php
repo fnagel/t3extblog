124231409e12172020444ca2814529c6c54093d0
Felix Nagel <info@felixnagel.com>
2026-07-02 18:19:45 +0200

    [TASK] Remove unneeded import

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,6 @@
 <?php
 
 use FelixNagel\T3extblog\Controller\TagController;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 use FelixNagel\T3extblog\Controller\PostController;
 use FelixNagel\T3extblog\Controller\CommentController;
