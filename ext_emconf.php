7df850931641b56da39dac796dc3a4127ddf68db
Felix Nagel <info@felixnagel.com>
2026-07-05 19:31:30 +0200

    [TASK] Add PHP 8.5 to test matrix

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.2.2-dev',
     'constraints' => [
         'depends' => [
-            'php' => '8.2.0-8.4.99',
+            'php' => '8.2.0-8.5.99',
             'typo3' => '14.2.0-14.3.99',
             'dashboard' => '',
         ],
